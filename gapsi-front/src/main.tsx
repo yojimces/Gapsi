324bf46c886bfc3ed7dee25ea955361155b55b24
yojimces <68842508+yojimces@users.noreply.github.com>
2025-11-02 19:11:51 -0500

    Initial commit - add main.tsx

---
+++ gapsi-front/src/main.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { store } from './redux/store'
+import App from './App'
+import './assets/styles/global.css'
+
+// PWA: Registrar Service Worker
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then(reg => console.log('SW registered:', reg))
+      .catch(err => console.log('SW registration failed:', err))
+  })
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+)
