51aa3a6c0e92cb1db848a58f38441860af81ff4e
yojimces <68842508+yojimces@users.noreply.github.com>
2025-11-02 19:11:23 -0500

    Initial commit - add service worker

---
+++ gapsi-front/public/sw.js
@@ -0,0 +1,40 @@
+// Service Worker para PWA
+const CACHE_NAME = 'gapsi-front-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/logo-gapsi.png'
+];
+
+// Instalación del Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Activación del Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Fetch del Service Worker
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        return response || fetch(event.request);
+      })
+  );
+});
